e2e7a6c3dc56aa57ca6b37d785cb52aa85a53b2a
jose manuel <josemanuelpiro@gmail.com>
2020-06-24 05:57:02 -0300

    .

--- src/main/java/Gendat.java
+++ src/main/java/Gendat.java
@@ -15,7 +15,6 @@ public class Gendat{
 
 
 
-
         /** 3)
          * la vista al recibir que se agregara un nuevo usuario, le pregunta a la base de datos si
          * tiene algun usurio con ese nombre, en caso de que no lo tenga agrega este nuevo usuario, en caso
